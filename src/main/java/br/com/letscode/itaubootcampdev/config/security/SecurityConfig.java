df4ede43ad4a55e96a5f2756a127f71bdb318a04
gabrieloswaldo <gabriel.oswaldo99@gmail.com>
2022-07-01 14:54:28 -0300

    Add endpoints authorization

--- src/main/java/br/com/letscode/itaubootcampdev/config/security/SecurityConfig.java
+++ src/main/java/br/com/letscode/itaubootcampdev/config/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package br.com.letscode.itaubootcampdev.config.security;
 
+import br.com.letscode.itaubootcampdev.enumeration.UserRoleEnum;
 import br.com.letscode.itaubootcampdev.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -22,6 +23,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/api/v1/movies").permitAll()
                 .antMatchers(HttpMethod.POST, "/api/v1/auth/*").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/v1/scores").hasAnyRole(
+                        UserRoleEnum.READER.name(), UserRoleEnum.BASIC.name(), UserRoleEnum.ADVANCED.name(), UserRoleEnum.ADMIN.name())
+                .antMatchers(HttpMethod.POST, "/api/v1/comments").hasAnyRole(
+                        UserRoleEnum.BASIC.name(), UserRoleEnum.ADVANCED.name(), UserRoleEnum.ADMIN.name())
+                .antMatchers(HttpMethod.DELETE, "/api/v1/comments/*").hasRole(UserRoleEnum.ADMIN.name())
+                .antMatchers(HttpMethod.POST, "/api/v1/replies").hasAnyRole(
+                        UserRoleEnum.BASIC.name(), UserRoleEnum.ADVANCED.name(), UserRoleEnum.ADMIN.name())
                 .anyRequest().authenticated()
                 .and().csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
---
+++ src/main/java/br/com/letscode/itaubootcampdev/enumeration/UserRoleEnum.java
@@ -0,0 +1,6 @@
+package br.com.letscode.itaubootcampdev.enumeration;
+
+public enum UserRoleEnum {
+
+    READER, BASIC, ADVANCED, ADMIN;
+}
